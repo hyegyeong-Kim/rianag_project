b76ec2dc6de0a8befbbf0842ea9e9fcb74b74ecd
hgkim <hgkim@rianag.com>
2021-12-20 16:39:23 +0900

    211220

--- SK_tid/resource/js/common.js
+++ SK_tid/resource/js/common.js
@@ -268,3 +268,68 @@ $(function(){
         })
     }
 })
+
+/* 통계 */
+// plugin
+const getOrCreateLegendList = (chart, id) => {
+    const legendContainer = document.getElementById(id);
+    let listContainer = legendContainer.querySelector('ul');
+
+    if (!listContainer) {
+        listContainer = document.createElement('ul');
+
+        legendContainer.appendChild(listContainer);
+    }
+
+    return listContainer;
+};
+
+const htmlLegendPlugin = {
+    id: 'htmlLegend',
+    afterUpdate(chart, args, options) {
+        const ul = getOrCreateLegendList(chart, options.containerID);
+
+        // Remove old legend items
+        while (ul.firstChild) {
+            ul.firstChild.remove();
+        }
+
+    // Reuse the built-in legendItems generator
+    const items = chart.options.plugins.legend.labels.generateLabels(chart);
+
+    items.forEach(item => {
+        const li = document.createElement('li');
+        li.style.alignItems = 'center';
+        li.style.cursor = 'pointer';
+
+        li.onclick = () => {
+            const {type} = chart.config;
+            if (type === 'pie' || type === 'doughnut') {
+                // Pie and doughnut charts only have a single dataset and visibility is per item
+                chart.toggleDataVisibility(item.index);
+            } else {
+                chart.setDatasetVisibility(item.datasetIndex, !chart.isDatasetVisible(item.datasetIndex));
+            }
+            chart.update();
+        };
+
+        // Color box
+        const boxSpan = document.createElement('span');
+        boxSpan.style.background = item.fillStyle;
+        boxSpan.style.borderColor = item.strokeStyle;
+        boxSpan.style.borderWidth = item.lineWidth + 'px';
+        boxSpan.classList.add('legend_bul');
+
+        // Text
+        const textContainer = document.createElement('span');
+        textContainer.classList.add('legend_txt');
+
+        const text = document.createTextNode(item.text);
+        textContainer.appendChild(text);
+
+        li.appendChild(boxSpan);
+        li.appendChild(textContainer);
+        ul.appendChild(li);
+    });
+    }
+};
